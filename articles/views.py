6e10ba26ecbf369ccfc9577da0b0d42d029501c5
AJAKR00KJ <ajaykr00kj@gmail.com>
2020-01-26 16:11:15 +0530

    first commit

---
+++ articles/views.py
@@ -0,0 +1,21 @@
+# from django.shortcuts import render
+#
+# # Create your views here.
+# def article_list(request):
+#     return (request,'articles/article_list.html')
+
+from django.shortcuts import render
+from .models import Articles
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+def article_list(request):
+    articles = Articles.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html' , { 'articles': articles })
+def article_detail(request,slug):
+    article = Articles.objects.get(slug=slug)
+    return render(request,'articles/article_detail.html',{'article':article})
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request,'articles/article_create.html')
